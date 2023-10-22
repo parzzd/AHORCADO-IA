b6c1733499f229e0df31d54282cbd128eab639ca
Pedro RD <pedro.ard1212@gmail.com>
2023-10-22 12:19:26 -0500

    update_adivinanza

--- adivinanzas.py
+++ adivinanzas.py
@@ -7,7 +7,7 @@ import tkinter as tk
 # kohonen_network = cargar_red_entrenada()
 
 # Definir las palabras o frases para adivinar
-palabras_a_adivinar = ["OTORRINOLARINGOLOGO", "NEUROCIRUJANO", "MATEMATICO", "PROFESOR", "PROGRAMADOR", "FOTOGRAFO"]
+palabras_a_adivinar = ["PERRO","GATO","RATA"]
 
 
 class JuegoAdivinanza:
@@ -21,6 +21,9 @@ class JuegoAdivinanza:
         
     def adivinar_letra(self, letra):
         #se encuentra palabra
+        if letra==self.palabra:
+            self.palabra_en_juego=self.palabra
+            return "!Ganaste¡"
         if letra in self.palabra:
             for i in range(len(self.palabra)):
                 if self.palabra[i] == letra:
@@ -30,9 +33,10 @@ class JuegoAdivinanza:
         #no se encuentra
         else:
             self.intentos -= 1
-            dibujar_ahorcado()
+            #self.dibujar_ahorcado()
             if self.intentos == 0:
                 return "¡Perdiste! La palabra era: " + self.palabra
+            
         return "".join(self.palabra_en_juego)
     
    
@@ -42,39 +46,55 @@ class JuegoAdivinanza:
         # Aquí debes consultar la red de Kohonen y obtener la letra más probable
         # basada en la pista actual
         pass
-def dibujar_ahorcado():
-    pass
+    def dibujar_ahorcado():
+        pass
+
 
 
 
 class InterfazJuego:
     def __init__(self, ventana):
+        #inicializador de variables generales
         self.ventana = ventana
-        self.ventana.title("JUEGO DE ADIVINANZA")
         self.juego = JuegoAdivinanza()
-
+        ###################################
+        self.ventana.title("JUEGO DE ADIVINANZA")
+        ###################################
+        #label PALABRA/GANASTE,PERDISTE
         self.label_palabra_juego = tk.Label(ventana, text=" ".join(self.juego.palabra_en_juego))
         self.label_palabra_juego.pack(fill=tk.X)
-
+        ###################################
+        #label VIDAS
         self.label_vidas = tk.Label(ventana, text=f"vidas: {self.juego.intentos}")
         self.label_vidas.pack(fill=tk.X)
-
+        ###################################
+        #INGRESAR LETRA
         self.ingresar_letra = tk.Entry(ventana)
         self.ingresar_letra.pack()
-
+        ###################################
+        #BUTON DE ACTIVACION
         self.boton_adivinar = tk.Button(ventana, text="Adivinar", command=self.adivinar)
         self.boton_adivinar.pack()
 
     def adivinar(self):
         letra = self.ingresar_letra.get().upper()
+        resultado = self.juego.adivinar_letra(letra)
         if letra in self.juego.letras_adivinadas:
             return
+    
+        if letra==self.juego.palabra:
+            self.label_palabra_juego.config(text=resultado)
+
         self.juego.letras_adivinadas.append(letra)
-        resultado = self.juego.adivinar_letra(letra)
         self.label_palabra_juego.config(text=resultado)
         self.label_vidas.config(text=f"vidas: {self.juego.intentos}")
+        #print(self.juego.letras_adivinadas)
+
+
 
 ventana = tk.Tk()
 app = InterfazJuego(ventana)
-ventana.geometry("500x500")
+ancho=ventana.winfo_height()
+alto=ventana.winfo_width()
+ventana.geometry(f"900x500+{ancho+80}+{alto}")
 ventana.mainloop()
