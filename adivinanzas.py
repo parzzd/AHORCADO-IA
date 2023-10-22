a1c8fdbaed8522125f75e21740c5cbd5af8b1d82
Pedro RD <pedro.ard1212@gmail.com>
2023-10-22 15:55:57 -0500

    codigo_con_dibujo
    se dibuja el juego del ahorcado

--- adivinanzas.py
+++ adivinanzas.py
@@ -1,78 +1,88 @@
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
 import tkinter as tk
+from tkinter import *
 
-# Carga la red de Kohonen pre-entrenada
-# kohonen_network = cargar_red_entrenada()
-
-# Definir las palabras o frases para adivinar
-palabras_a_adivinar = ["PERRO","GATO","RATA"]
+# Define the words or phrases for guessing
+palabras_a_adivinar = ["PERRO", "GATO", "RATA"]
 
 
 class JuegoAdivinanza:
-    def __init__(self):
+    def __init__(self, canvas):
         self.palabra = np.random.choice(palabras_a_adivinar)
-        self.intentos = 3
+        self.intentos = 5
         self.letras_adivinadas = []
-        self.palabra_en_juego =  ["_" for i in  range(len(self.palabra))]
+        self.palabra_en_juego = ["_" for _ in range(len(self.palabra))]
+        self.canvas = canvas  
 
-    
+    def dibujar_ahorcado(self):
+        if self.intentos==4:
+        #lineas
+            self.canvas.create_line(350, 60, 350, 400, width=10, fill='green')#|
+            self.canvas.create_line(345, 60, 500, 60, width=10, fill='green')#----
+            self.canvas.create_line(445, 60, 445, 120, width=10, fill='green')#|
+        elif self.intentos==3:
+        #jeta
+            self.canvas.create_oval(400, 100, 500, 200, width=2, fill='black')
+        elif self.intentos==2:
+        #cuerpo
+            self.canvas.create_line(445, 195, 445, 300, width=10, fill='black')#|
+        elif self.intentos==1:
+        #piernas
+            self.canvas.create_line(445, 300, 395, 335, width=10, fill='black')#|
+            self.canvas.create_line(445, 300, 495, 335, width=10, fill='black')#|
+        #brazos
         
+            self.canvas.create_line(445, 220, 395, 255, width=10, fill='black')#|
+            self.canvas.create_line(445, 220, 495, 255, width=10, fill='black')#|
+        elif self.intentos==0:            
+        #cara
+            #ojo izq
+            self.canvas.create_line(445, 125, 425, 135, width=4, fill='white')#X
+            self.canvas.create_line(425, 125, 445, 135, width=4, fill='white')#X
+            #ojo der
+            self.canvas.create_line(475, 125, 455, 135, width=4, fill='white')#X
+            self.canvas.create_line(455, 125, 475, 135, width=4, fill='white')#X    
+            #boca
+            self.canvas.create_line(430, 170, 470, 170, width=5, fill='white')#-
+            
     def adivinar_letra(self, letra):
-        #se encuentra palabra
-        if letra==self.palabra:
-            self.palabra_en_juego=self.palabra
-            return "!Ganaste¡"
+        if letra == self.palabra:
+            self.palabra_en_juego = list(self.palabra)
+            return "¡Ganaste!"
+
         if letra in self.palabra:
             for i in range(len(self.palabra)):
                 if self.palabra[i] == letra:
                     self.palabra_en_juego[i] = letra
             if "_" not in self.palabra_en_juego:
                 return "¡Ganaste!"
-        #no se encuentra
         else:
             self.intentos -= 1
-            #self.dibujar_ahorcado()
+            self.dibujar_ahorcado()
             if self.intentos == 0:
                 return "¡Perdiste! La palabra era: " + self.palabra
-            
+
         return "".join(self.palabra_en_juego)
-    
-   
-
-
-    def obtener_pista_kohonen(self):
-        # Aquí debes consultar la red de Kohonen y obtener la letra más probable
-        # basada en la pista actual
-        pass
-    def dibujar_ahorcado():
-        pass
-
-
 
 
 class InterfazJuego:
     def __init__(self, ventana):
-        #inicializador de variables generales
         self.ventana = ventana
-        self.juego = JuegoAdivinanza()
-        ###################################
+        self.canvas = Canvas(width=300, height=210, bg='white')
+        self.canvas.pack(expand=YES, fill=BOTH)
+        self.juego = JuegoAdivinanza(self.canvas)
+
         self.ventana.title("JUEGO DE ADIVINANZA")
-        ###################################
-        #label PALABRA/GANASTE,PERDISTE
+
         self.label_palabra_juego = tk.Label(ventana, text=" ".join(self.juego.palabra_en_juego))
         self.label_palabra_juego.pack(fill=tk.X)
-        ###################################
-        #label VIDAS
+
         self.label_vidas = tk.Label(ventana, text=f"vidas: {self.juego.intentos}")
         self.label_vidas.pack(fill=tk.X)
-        ###################################
-        #INGRESAR LETRA
+
         self.ingresar_letra = tk.Entry(ventana)
         self.ingresar_letra.pack()
-        ###################################
-        #BUTON DE ACTIVACION
+
         self.boton_adivinar = tk.Button(ventana, text="Adivinar", command=self.adivinar)
         self.boton_adivinar.pack()
 
@@ -81,20 +91,16 @@ class InterfazJuego:
         resultado = self.juego.adivinar_letra(letra)
         if letra in self.juego.letras_adivinadas:
             return
-    
-        if letra==self.juego.palabra:
-            self.label_palabra_juego.config(text=resultado)
 
         self.juego.letras_adivinadas.append(letra)
         self.label_palabra_juego.config(text=resultado)
         self.label_vidas.config(text=f"vidas: {self.juego.intentos}")
-        #print(self.juego.letras_adivinadas)
-
 
 
 ventana = tk.Tk()
 app = InterfazJuego(ventana)
-ancho=ventana.winfo_height()
-alto=ventana.winfo_width()
-ventana.geometry(f"900x500+{ancho+80}+{alto}")
+ancho = ventana.winfo_height()
+alto = ventana.winfo_width()
+ventana.geometry(f"900x500+{ancho + 80}+{alto}")
+
 ventana.mainloop()
